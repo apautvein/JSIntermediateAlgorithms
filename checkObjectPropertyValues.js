9c0bd0e230063f2ca91983c5175047a21106b58f
apautvein <apautvein@gmail.com>
2020-06-12 11:27:52 -0500

    return true/false based on object property values

---
+++ checkObjectPropertyValues.js
@@ -0,0 +1,5 @@
+const truthCheck = (collection, pre) =>
+  collection.every((user) => user[pre]);
+
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
